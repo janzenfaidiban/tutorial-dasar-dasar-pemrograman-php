f21039f2890f5043ee1b26789c2610090c390844
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-07-25 18:47:56 +0900

    tambahkan echo-print.php

---
+++ echo-print.php
@@ -0,0 +1,9 @@
+<?php 
+	// Contoh penggunaan echo
+	echo "<h1>Papua Tech Community</h1>";
+	echo "<p>Berbagi dan belajar bersama di komunitas.</p>";
+
+	// Contoh penggunaan print
+	print "<h1>Papua Coding School</h1>";
+	print "<p>Belajar coding dari dasar dengan mengikuti kursus.</p>";
+?>
