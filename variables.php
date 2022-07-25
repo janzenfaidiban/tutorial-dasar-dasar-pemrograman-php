ad2e105185c06db064c6cbb6ba8ac5c36bcb3074
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-07-25 18:42:52 +0900

    tambah variables.php

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php 
+	// Contoh penulisan variables di dalam file PHP
+	$title = "SaCode";
+	$tag_line = "Papua Coding School";
+	$tahun = "2022";
+
+	// Menampilkan data dari dalam variable
+	echo $title;
+	echo "<br>";
+
+	echo $tag_line;
+	echo "<br>";
+
+	echo $tahun;
+?>
